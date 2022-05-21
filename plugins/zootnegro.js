6280fc5cb49e3455f689b2f9a06d117fb5b34389
AsianBotV1 <104330520+AsianBotV1@users.noreply.github.com>
2022-05-21 13:19:37 -0500

    Update zootnegro.js

--- plugins/zootnegro.js
+++ plugins/zootnegro.js
@@ -1,11 +1,11 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
     let info = `
 *⍣Zoot Revo⍣*
-https://www.youtube.com/channel/UC5QIa0PWbCCUUTJa62VDo_g
+https://www.youtube.com/channel
 Canal de buzoneo y metodos private
     `.trim() 
     
-    conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 𝐉𝐡𝐮𝐬𝐳ᙆ  ᷦ×͜×*', 'status@broadcast')
+    conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 AsianBot  ᷦ×͜×*', 'status@broadcast')
     }
     handler.command = /^(zoot|ZootRevo|el ma negro)$/i
     
