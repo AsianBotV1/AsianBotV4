4066f15d118c8a99a20b74e7f748fcd2b7650b46
AsianBotV1 <104330520+AsianBotV1@users.noreply.github.com>
2022-05-21 12:12:40 -0500

    Update igofc.js

--- plugins/igofc.js
+++ plugins/igofc.js
@@ -1,10 +1,10 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
     let info = `
     *Hola 👋🏻, aquí encontrarás el Instagram oficial de mi owner*
-*https://www.instagram.com/idk_jhuz*
+*https://www.instagram.com*
     `.trim() 
     
-    conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 𝐉𝐡𝐮𝐬𝐳ᙆ  ᷦ×͜× 🔥*', 'status@broadcast')
+    conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 bill  ᷦ×͜× 🔥*', 'status@broadcast')
     }
     handler.command = /^(ig|igofc|instagramofc)$/i
     
